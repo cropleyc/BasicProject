bbcd345a5e63132078c1db34c33ba7b07d24662b
Chad Cropley <cropleyc@oregonstate.edu>
2020-07-20 12:05:55 -0700

    Add project files.

---
+++ BasicProject/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\cropleyc\Documents\Projects\BasicProject\BasicProject\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
